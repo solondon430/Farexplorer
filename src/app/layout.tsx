5aa8f5d10c70b1e4e344ff00c067cd77b59ad35c
solondon430 <solondon430@gmail.com>
2026-02-01 09:30:37 +0700

    update frame domain

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,6 +54,6 @@ export default function RootLayout({
 export const metadata: Metadata = {
         title: "Farcaster Explorer",
         description: "Explore Farcaster profiles, Share your Neynar Score and discover your influence level .",
-        other: { "fc:frame": JSON.stringify({"version":"next","imageUrl":"https://usdozf7pplhxfvrl.public.blob.vercel-storage.com/thumbnail_cmij0eo7d000004l1bn7b1wrq-BXT3HaSncdvzHnUxepVYVn72sLeiqn","button":{"title":"Check Profile","action":{"type":"launch_frame","name":"Farcaster Explorer","url":"https://story-strip-906.app.ohara.ai","splashImageUrl":"https://usdozf7pplhxfvrl.public.blob.vercel-storage.com/farcaster/splash_images/splash_image1.svg","splashBackgroundColor":"#ffffff"}}}
+        other: { "fc:frame": JSON.stringify({"version":"next","imageUrl":"https://usdozf7pplhxfvrl.public.blob.vercel-storage.com/thumbnail_cmij0eo7d000004l1bn7b1wrq-BXT3HaSncdvzHnUxepVYVn72sLeiqn","button":{"title":"Check Profile","action":{"type":"launch_frame","name":"Farcaster Explorer","url":"https://farexplorer-3iwa.vercel.app","splashImageUrl":"https://usdozf7pplhxfvrl.public.blob.vercel-storage.com/farcaster/splash_images/splash_image1.svg","splashBackgroundColor":"#ffffff"}}}
         ) }
     };
